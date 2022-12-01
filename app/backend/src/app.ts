a4154ac33198a8937b7b63b057b29d502917104b
JoaoDobrowolski <joaodobrowolski@outlook.com>
2022-11-30 23:05:10 -0300

    ué

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -11,7 +11,7 @@ class App {
 
     this.config();
 
-    // Não remover essa rota!
+    // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
 
     this.app.use('/login', loginRoute);
